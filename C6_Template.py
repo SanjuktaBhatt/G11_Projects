435163bc26b759b4a185a2f4cce3d228c47cbf43
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-07-22 14:07:56 +0530

    Update C6_Template.py

--- C6_Template.py
+++ C6_Template.py
@@ -9,13 +9,11 @@ YELLOW = (255,255,0)
 GREEN=(0,255,0)
 BROWN=(165,102,42)
 size = (400, 400)
+#Use list comprehension to creat a list of bricks for the wall
 
 
-#Use list comprehension to create a list of orange row
 
-#Use list comprehension to create a list of white row
 
-#Use list comprehension to create a list of green row
 
 
 screen = pygame.display.set_mode(size)
@@ -29,26 +27,23 @@ while carryOn:
   pygame.draw.line(screen, WHITE, [88,0], [88,400], 4)
   pygame.draw.line(screen, WHITE, [82,0], [82,400], 4)
   font = pygame.font.Font(None, 34)
-  text = font.render("Independence Day", 1, DARKBLUE)
-  text2 = font.render("Celebration", 1, DARKBLUE)
-  bgd=pygame.Rect(150,10,210,40)
+  text = font.render("Sports Day", 1, DARKBLUE)
+
+  bgd=pygame.Rect(150,10,130,20)
   pygame.draw.rect(screen,WHITE,bgd)
   screen.blit(text, (150,10))
-  screen.blit(text2, (180,30))
+  
   for i in range(5,80,20):
     for j in range(5,391,20):
       audience=pygame.Rect(i,j,10,10)
       pygame.draw.rect(screen,(0,0,0),audience)
-  #Create a for loop to draw each row
- 
-
-
-
-
-
-
-
-  chakra=pygame.Rect(225,200,20,20)
-  pygame.draw.rect(screen,DARKBLUE,chakra)
+  #Create a for loop to draw each brick
+  
+  
+  
+  
+  
+  
+  
   pygame.display.flip()
 pygame.quit()
